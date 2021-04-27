6641a6b24ddcfc8ac3d764a7c2f72a9cec288910
wandakim <nwd1583@naver.com>
2021-04-27 23:24:49 +0900

    Coordinate chaser - better performance without layout, paint

--- Web APIs/coordinates/main.js
+++ Web APIs/coordinates/main.js
@@ -6,16 +6,16 @@ const verti = document.querySelector('.vertical');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-
-
-window.addEventListener('mousemove', (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    hori.style.top = `${y}px`;
-    verti.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.innerHTML = `${x}px, ${y}px`;
-})
+addEventListener('load',() => { // load 로 하지 않으면 Rect가 제대로 불러와 지지않는다. css, 이미지가 다 다운로드 된 상태에서 실행하게 함. 
+    const targetHarfWidth = target.getBoundingClientRect().width / 2;
+    const targetHarfHeight = target.getBoundingClientRect().height / 2;
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        hori.style.transform = `translate(0,${y}px)`;
+        verti.style.transform = `translate(${x}px,0`;
+        target.style.transform = `translate(${x-targetHarfWidth}px,${y-targetHarfHeight}px)`;
+        tag.style.transform = `translate(${x}px,${y}px)`;
+        tag.innerHTML = `${x}px, ${y}px`;
+    })
+});
